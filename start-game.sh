552da030108dbe1e0da1a6ba4a5f068e4717921a
peter <to.petr@gmail.com>
2024-02-04 10:48:21 +0100

    update startup script

--- start-game.sh
+++ start-game.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cp -r ~/dev/minetest-mods/playmod ~/Library/Containers/mobi.MultiCraft/Data/Library/Application\ Support/multicraft/mods
+mkdir -p ~/Library/Containers/mobi.MultiCraft/Data/Library/Application\ Support/multicraft/worlds/multicraftw/worldmods
 cp -r ~/dev/minetest-mods/stoneblocks ~/Library/Containers/mobi.MultiCraft/Data/Library/Application\ Support/multicraft/worlds/multicraftw/worldmods/
 /Applications/MultiCraft.app/Contents/MacOS/MultiCraft --info
