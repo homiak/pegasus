bebc2d40b04b82427d8651a8354ca0e4d92a290e
Margo <margo.troshin@gmail.com>
2024-03-07 19:30:20 +0100

    add fire starter tools

--- colorflames/crafts.lua
+++ colorflames/crafts.lua
@@ -8,3 +8,43 @@ minetest.register_craftitem("colorflames:orange_fire_starter_tool", {
     description = "Orange fire starter tool",
     inventory_image = "orange_fire_starter_tool.png"
 })
+
+minetest.register_craftitem("colorflames:blue_fire_starter_tool", {
+    description = "Blue fire starter tool",
+    inventory_image = "blue_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:red_fire_starter_tool", {
+    description = "Red fire starter tool",
+    inventory_image = "red_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:rose_fire_starter_tool", {
+    description = "Rose fire starter tool",
+    inventory_image = "rose_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:black_fire_starter_tool", {
+    description = "Black fire starter tool",
+    inventory_image = "black_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:grey_fire_starter_tool", {
+    description = "Grey fire starter tool",
+    inventory_image = "grey_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:green_fire_starter_tool", {
+    description = "Green fire starter tool",
+    inventory_image = "green_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:cyan_fire_starter_tool", {
+    description = "Cyan fire starter tool",
+    inventory_image = "cyan_fire_starter_tool.png"
+})
+
+minetest.register_craftitem("colorflames:yellow_fire_starter_tool", {
+    description = "Yellow fire starter tool",
+    inventory_image = "yellow_fire_starter_tool.png"
+})
