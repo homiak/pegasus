d0bc1d4c62dec4a3663e7697230cd2a784acf6b0
Margo <margo.troshin@gmail.com>
2024-03-08 21:17:42 +0100

    add code for all flints

--- colorflames/crafts.lua
+++ colorflames/crafts.lua
@@ -1,68 +1,38 @@
-minetest.register_craft({
-	type = "shapeless",
-	output = "colorflames:orange_fire_starter_tool",
-	recipe = { "fire:flint_and_steel", "dye:orange_dye" }
 
-})
+local flint_colors = {
+	"green", "yellow", "black", "orange", "cyan", "blue", "red"
+}
 
-minetest.register_craftitem("colorflames:orange_fire_starter_tool", {
-    description = "Orange fire starter tool",
-    inventory_image = "orange_fire_starter_tool.png",
-    on_use = function(itemstack, user, pointed_thing)
-        if pointed_thing.type == "node" then
-            -- Get the above node position
-            local pos = minetest.get_pointed_thing_position(pointed_thing, above)
-            local node = minetest.get_node(pos)
-            pos.y = pos.y + 1 -- Adjust the position to be above the pointed node
-            local above_node = minetest.get_node(pos)
-            -- Check if the position above is air
-            if above_node.name == "air" then
-                -- Set fire to the node, replace "colorflames:yellow_fire" with the correct fire node name in your game
-                minetest.set_node(pos, { name = "colorflames:orange_fire"})
-            end    
-            -- Optionally, reduce the item's wear or remove one item from the stack
-            -- itemstack:take_item()
-            return itemstack
-        end
-    end,
-})
+local function register_flint_craft(color) 
+    minetest.register_craft({
+        type = "shapeless",
+        output = "colorflames:".. color .. "_fire_starter_tool",
+        recipe = { "fire:flint_and_steel", "dye:" .. color }    
+    })
 
-minetest.register_craftitem("colorflames:blue_fire_starter_tool", {
-    description = "Blue fire starter tool",
-    inventory_image = "blue_fire_starter_tool.png"
-})
+    minetest.register_craftitem("colorflames:" .. color .. "_fire_starter_tool", {
+        description = color .. " fire starter tool", -- capital
+        inventory_image = color .. "_fire_starter_tool.png",
+        on_use = function(itemstack, user, pointed_thing)
+            if pointed_thing.type == "node" then
+                -- Get the above node position
+                local pos = minetest.get_pointed_thing_position(pointed_thing, above)
+                local node = minetest.get_node(pos)
+                pos.y = pos.y + 1 -- Adjust the position to be above the pointed node
+                local above_node = minetest.get_node(pos)
+                -- Check if the position above is air
+                if above_node.name == "air" then
+                    -- Set fire to the node, replace "colorflames:yellow_fire" with the correct fire node name in your game
+                    minetest.set_node(pos, { name = "colorflames:".. color .."_fire"})
+                end    
+                -- Optionally, reduce the item's wear or remove one item from the stack
+                -- itemstack:take_item()
+                return itemstack
+            end
+        end,
+    })
+end
 
-minetest.register_craftitem("colorflames:red_fire_starter_tool", {
-    description = "Red fire starter tool",
-    inventory_image = "red_fire_starter_tool.png"
-})
-
-minetest.register_craftitem("colorflames:rose_fire_starter_tool", {
-    description = "Rose fire starter tool",
-    inventory_image = "rose_fire_starter_tool.png"
-})
-
-minetest.register_craftitem("colorflames:black_fire_starter_tool", {
-    description = "Black fire starter tool",
-    inventory_image = "black_fire_starter_tool.png"
-})
-
-minetest.register_craftitem("colorflames:grey_fire_starter_tool", {
-    description = "Grey fire starter tool",
-    inventory_image = "grey_fire_starter_tool.png"
-})
-
-minetest.register_craftitem("colorflames:green_fire_starter_tool", {
-    description = "Green fire starter tool",
-    inventory_image = "green_fire_starter_tool.png"
-})
-
-minetest.register_craftitem("colorflames:cyan_fire_starter_tool", {
-    description = "Cyan fire starter tool",
-    inventory_image = "cyan_fire_starter_tool.png"
-})
-
-minetest.register_craftitem("colorflames:yellow_fire_starter_tool", {
-    description = "Yellow fire starter tool",
-    inventory_image = "yellow_fire_starter_tool.png"
-})
+for _, color in pairs(flint_colors) do
+    register_flint_craft(color)
+end
